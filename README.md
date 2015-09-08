1a1e5a86e99c55b73d0269d321b425e970ead105
白植雁 <549358950.com>
2015-09-08 10:23:17 +0800

    后台

---
+++ README.md
@@ -0,0 +1,1 @@
+#fuerdai_web
---
+++ goose/views.py
@@ -0,0 +1,121 @@
+from django.shortcuts import render
+
+def accusation_deal(request):
+    return render(request, "fuerdai/accusation_deal.html", {})
+
+def accusation(request):
+    return render(request, "fuerdai/accusation.html", {})
+
+def add_company(request):
+    return render(request, "fuerdai/add_company.html", {})
+
+def add_shop(request):
+    return render(request, "fuerdai/add_shop.html", {})
+
+def add_temp(request):
+    return render(request, "fuerdai/add_temp.html", {})
+
+def add_tickling(request):
+    return render(request, "fuerdai/add_tickling.html", {})
+
+def add_unattuned_shop(request):
+    return render(request, "fuerdai/add_unattuned_shop.html", {})
+
+def add_waiter(request):
+    return render(request, "fuerdai/add_waiter.html", {})
+
+def address(request):
+    return render(request, "fuerdai/address.html", {})
+
+def app_maintain(request):
+    return render(request, "fuerdai/app_maintain.html", {})
+
+def approval_addcompany(request):
+    return render(request, "fuerdai/approval_addcompany.html", {})
+
+def approval_addshop(request):
+    return render(request, "fuerdai/approval_addshop.html", {})
+
+def approval_altercompany(request):
+    return render(request, "fuerdai/approval_altercompany.html", {})
+
+def approval_altershop(request):
+    return render(request, "fuerdai/approval_altershop.html", {})
+
+def company_item(request):
+    return render(request, "fuerdai/company_item.html", {})
+
+def company(request):
+    return render(request, "fuerdai/company.html", {})
+
+def consumer_orders(request):
+    return render(request, "fuerdai/consumer_orders.html", {})
+    
+def crowdfunding_orders(request):
+    return render(request, "fuerdai/crowdfunding_orders.html", {})
+
+def explain(request):
+    return render(request, "fuerdai/explain.html", {})
+
+def explain_edit(request):
+    return render(request, "fuerdai/explain_edit.html", {})
+
+def data(request):
+    return render(request, "fuerdai/data.html", {})
+
+def feereturn(request):
+    return render(request, "fuerdai/feereturn.html", {})
+
+def feedback(request):
+    return render(request, "fuerdai/feedback.html", {})
+
+def forget(request):
+    return render(request, "fuerdai/forget.html", {})
+
+def index(request):
+    return render(request, "fuerdai/index.html", {})
+
+def maintain(request):
+    return render(request, "fuerdai/maintain.html", {})
+    
+def pay_record(request):
+    return render(request, "fuerdai/pay_record.html", {})
+
+def purse(request):
+    return render(request, "fuerdai/purse.html", {})
+
+def refund_record(request):
+    return render(request, "fuerdai/refund_record.html", {})
+
+def unattuned_shop(request):
+    return render(request, "fuerdai/unattuned_shop.html", {})
+
+def shop_item(request):
+    return render(request, "fuerdai/shop_item.html", {})
+
+def sort(request):
+    return render(request, "fuerdai/sort.html", {})
+
+def temp(request):
+    return render(request, "fuerdai/temp.html", {})
+
+def temp_item(request):
+    return render(request, "fuerdai/temp_item.html", {})
+
+def tickling(request):
+    return render(request, "fuerdai/tickling.html", {})
+
+def waiter_item(request):
+    return render(request, "fuerdai/waiter_item.html", {})
+
+def waiter(request):
+    return render(request, "fuerdai/waiter.html", {})
+
+def waiter_bill(request):
+    return render(request, "fuerdai/waiter_bill.html", {})
+
+def waiter_pay(request):
+    return render(request, "fuerdai/waiter_pay.html", {})
+
+def informality_waiter(request):
+    return render(request, "fuerdai/informality_waiter.html", {})
